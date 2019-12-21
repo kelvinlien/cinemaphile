7c87eb211b601ca06961d8a641d6e0e065a0c7bd
Kelvin <Mr.Secret0987@gmail.com>
2019-12-21 22:37:37 +0700

    feat: add shimmerview for loading screen

--- app/src/main/java/com/example/myapplication/detailmovie/DetailMovieActivity.java
+++ app/src/main/java/com/example/myapplication/detailmovie/DetailMovieActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication.detailmovie;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,8 +22,10 @@ import com.example.myapplication.network.HttpUtils;
 
 import org.json.*;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.loopj.android.http.*;
 
+import com.facebook.shimmer.*;
 import java.util.concurrent.ExecutionException;
 
 public class DetailMovieActivity extends Activity {
@@ -45,6 +49,8 @@ public class DetailMovieActivity extends Activity {
 
     HttpUtils client;
 
+    AlertDialog alertDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,6 +66,15 @@ public class DetailMovieActivity extends Activity {
         RottenTomatoes = findViewById(R.id.RottenTomatoes_score);
         Metacritic = findViewById(R.id.Metacritic_score);
 
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        ShimmerFrameLayout container =
+                (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
+        container.startShimmer();
     }
 
 
@@ -97,11 +112,13 @@ public class DetailMovieActivity extends Activity {
         RequestParams rp = new RequestParams();
 //        rp.add("username", "aaa"); rp.add("password", "aaa@123");
 
-        HttpUtils.getByUrl("http://www.omdbapi.com/?apikey=61222a7d&t=Avengers+ +Endgame", rp, new JsonHttpResponseHandler() {
+        HttpUtils.getByUrl("http://www.omdbapi.com/?apikey=61222a7d&t=Inglourious + +Basterds", rp, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 // If the response is JSONObject instead of expected JSONArray
                 Log.d("asd", "---------------- this is response : " + response);
+                ShimmerFrameLayout container =
+                        (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
                 try {
                     JSONObject serverResp = new JSONObject(response.toString());
                     String title = serverResp.getString("Title");
@@ -116,6 +133,8 @@ public class DetailMovieActivity extends Activity {
                     String roto_score = roto.getString("Value");
                     JSONObject meta = ratings.getJSONObject(2);
                     String meta_score = meta.getString("Value");
+                    String res = serverResp.getString("Response");
+                    checkResponse(res);
                     Bitmap bmp = null;
                     try {
                         bmp = new getImageFromURL().execute(poster).get();
@@ -124,14 +143,20 @@ public class DetailMovieActivity extends Activity {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    String[] temp = runtime.split(" ");
+                    int hours = Integer.parseInt(temp[0]) / 60;
+                    int minutes = Integer.parseInt(temp[0]) % 60;
+                    String duration = hours > 0 ? Integer.toString(hours) + "h" + Integer.toString(minutes) + "min" : Integer.toString(minutes) + "min";
                     Poster.setImageBitmap(bmp);
                     MovieName.setText((CharSequence) title);
                     Year.setText((CharSequence)year);
                     Rated.setText(rated);
-                    Runtime.setText(runtime);
+                    Runtime.setText(duration);
                     IMDb.setText(imdb_score);
                     RottenTomatoes.setText(roto_score);
                     Metacritic.setText(meta_score);
+                    container.stopShimmer();
+                    container.hideShimmer();
                 } catch (JSONException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
@@ -145,6 +170,19 @@ public class DetailMovieActivity extends Activity {
 
             }
         });
+    }
+
+    protected void checkResponse(String res) {
+        if (res.equals("False")) {
+            alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Not found");
+            alertDialog.setMessage("Movie name not found!");
+            // Set the Icon for the Dialog
+            alertDialog.setButton("Back", (dialog, which) -> {
+                // TODO Add your code for the button here.   }
+            });
+            alertDialog.show();
+        }
     }
 }
 
