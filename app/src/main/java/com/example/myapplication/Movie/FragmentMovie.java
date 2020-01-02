62f49451baf8f46da92f6a7faaf65818966f08c9
Kelvin <Mr.Secret0987@gmail.com>
2020-01-02 17:58:43 +0700

    feat: retrieving trending movies and show them

--- app/src/main/java/com/example/myapplication/Movie/FragmentMovie.java
+++ app/src/main/java/com/example/myapplication/Movie/FragmentMovie.java
@@ -11,11 +11,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.SearchResultsActivity;
 import com.example.myapplication.adapter.HomeAdapter;
 import com.example.myapplication.model.movie_item;
+import com.example.myapplication.network.TMDB;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class FragmentMovie extends Fragment {
@@ -23,6 +31,7 @@ public class FragmentMovie extends Fragment {
     HomeAdapter adapter;
     public List<movie_item>  movieList;
     RecyclerView recyclerView;
+
     public FragmentMovie() {
     }
 
@@ -32,6 +41,7 @@ public class FragmentMovie extends Fragment {
         View view = inflater.inflate(R.layout.movie_fragment, container, false);
         recyclerView=view.findViewById(R.id.rv_movie2);
         adapter=new HomeAdapter(movieList,getContext());
+        System.out.println("movieList to show is: " + movieList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
@@ -41,26 +51,52 @@ public class FragmentMovie extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       movieList=new ArrayList<>();
 
-        movieList.add(
-                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
-        );
-        movieList.add(
-                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
-        );
-        movieList.add(
-                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
-        );
-        movieList.add(
-                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
-        );
-        movieList.add(
-                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
-        );
-        movieList.add(
-                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
-        );
+        movieList=new ArrayList<>();
+//        movieList.add(
+//                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
+//        );
+//        movieList.add(
+//                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
+//        );
+//        movieList.add(
+//                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
+//        );
+//        movieList.add(
+//                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
+//        );
+//        movieList.add(
+//                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
+//        );
+//        movieList.add(
+//                new movie_item("1/1/2018","Maze Runner","R.drawable.maze_runner","this is movie was released in 2018")
+//        );
+        TMDB.getInstance().getPopularMovie(1, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onFailure(int a, Header[] hd, Throwable tw, JSONObject response)
+            {
+                System.out.println("popular movie call went wrong: " + response);
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    movieList = TMDB.getInstance().processResults(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                adapter=new HomeAdapter(movieList,getContext());
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+                System.out.println("current movieList is: " + movieList);
+            }
+        });
 
     }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+
 }
